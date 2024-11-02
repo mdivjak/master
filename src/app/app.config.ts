62c5e2c928ca4a0cd8db18ea8f311f1c8c10fae6
mdivjak <marko.d@europe.com>
2024-11-02 23:27:48 +0100

    setting up firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { firebaseConfig } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCu5CPQCRQ7_qsOst9yUHo9_VXxDZ7RQv0",
+    authDomain: "master-b6858.firebaseapp.com",
+    projectId: "master-b6858",
+    storageBucket: "master-b6858.firebasestorage.app",
+    messagingSenderId: "21300314672",
+    appId: "1:21300314672:web:8cd1730aa552dff5f3e3f5"
+};
