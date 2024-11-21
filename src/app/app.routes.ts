a08a561e240641457de59d953c02d50a7707e7ed
mdivjak <marko.d@europe.com>
2024-11-21 21:23:47 +0100

    adding auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileClubComponent } from './components/profile-club/profile-club.component';
 import { ProfileHikerComponent } from './components/profile-hiker/profile-hiker.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'profile-club', component: ProfileClubComponent },
-    { path: 'profile-hiker', component: ProfileHikerComponent }
+    { path: 'profile-club', component: ProfileClubComponent, canActivate: [authGuard] },
+    { path: 'profile-hiker', component: ProfileHikerComponent, canActivate: [authGuard] }
 ];
