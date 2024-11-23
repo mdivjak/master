346c2da27129a865ad6863336060bf24552751e1
mdivjak <marko.d@europe.com>
2024-11-23 19:24:23 +0100

    adding map component that is not working

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,37 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit, AfterViewInit{
+onMapReady($event: Event) {
+throw new Error('Method not implemented.');
+}
+  private map!: L.Map;
+
+  ngOnInit(): void {}
+  
+  ngAfterViewInit(): void {
+    this.initializeMap();
+  }
+
+  initializeMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+}
