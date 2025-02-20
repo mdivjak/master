53d89f3d7a5a76fcb2aa65afd4facbda159e5c0d
mdivjak <marko.d@europe.com>
2025-02-20 21:16:20 +0100

    refactor auth and tour services; add user data retrieval and update methods; remove unused user service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, UserCredential } from '@angular/fire/auth';
-import { doc, Firestore, getDoc, setDoc } from '@angular/fire/firestore';
+import { doc, Firestore, getDoc, setDoc, updateDoc } from '@angular/fire/firestore';
 import { onAuthStateChanged, signOut, User } from 'firebase/auth';
 import { BehaviorSubject } from 'rxjs';
 import { UserData } from '../models/userdata';
@@ -11,23 +11,25 @@ import { UserData } from '../models/userdata';
 export class AuthService {
   private auth = inject(Auth);
   private firestore = inject(Firestore);
+
   private userSubject = new BehaviorSubject<User | null>(null);
-  private userTypeSubject = new BehaviorSubject<string | null>(null);
-  private userDataSubject = new BehaviorSubject<UserData | null>(null);
+
   private _currentUserData: UserData | null = null;
+  private userDataSubject = new BehaviorSubject<UserData | null>(null);
+
+  private userTypeSubject = new BehaviorSubject<string | null>(null);
 
   constructor() {
     onAuthStateChanged(this.auth, async (user) => {
       if(user) {
         const userDoc = await getDoc(doc(this.firestore, 'users', user.uid));
-        
-        const userData = userDoc.data() as UserData;
-        this._currentUserData = userData;
-        this.userDataSubject.next(userData);
 
-        const userType = userData?.type ?? null;
-        this.userTypeSubject.next(userType);
+        this._currentUserData = userDoc.data() as UserData;
+        this.userDataSubject.next(this._currentUserData);
+        this.userTypeSubject.next(this._currentUserData.type ?? null);
       } else {
+        this._currentUserData = null;
+        this.userDataSubject.next(null);
         this.userTypeSubject.next(null);
       }
       this.userSubject.next(user);
@@ -38,33 +40,28 @@ export class AuthService {
     return this.userSubject.asObservable();
   }
 
-  get userType$() {
-    return this.userTypeSubject.asObservable();
-  }
-
   get userData$() {
     return this.userDataSubject.asObservable();
   }
 
-  get currentUserData() {
-    return this._currentUserData;
+  get userType$() {
+    return this.userTypeSubject.asObservable();
   }
 
   get currentUser() {
     return this.auth.currentUser;
   }
 
-  async register(photoString: string, email: string, password: string, name: string, userType: string) {
-    const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+  get currentUserData() {
+    return this._currentUserData;
+  }
+
+  async register(userData: UserData, password: string) {
+    const userCredential = await createUserWithEmailAndPassword(this.auth, userData.email, password);
     const user = userCredential.user;
 
     // Save user details in Firestore
-    await setDoc(doc(this.firestore, 'users', user.uid), {
-      name: name,
-      email: email,
-      type: userType,
-      photo: photoString
-    });
+    await setDoc(doc(this.firestore, 'users', user.uid), userData);
 
     return userCredential;
   }
@@ -77,6 +74,20 @@ export class AuthService {
     return await signOut(this.auth);
   }
 
+  async getUserData(userId: string) {
+    const userDoc = await getDoc(doc(this.firestore, "users", userId));
+    return userDoc.exists() ? userDoc.data() : null;
+  }
+
+  async updateUser(userId: string, name?: string, photo?: string) {
+    const updates: any = {};
+    if (name) updates.name = name;
+    if (photo) updates.photo = photo;
+  
+    await updateDoc(doc(this.firestore, "users", userId), updates);
+  }
+
+  //deprecate
   updateUserData(userProfile: UserData) {
     if(this.currentUser) {
       return setDoc(doc(this.firestore, 'users', this.currentUser.uid), userProfile);
--- src/app/services/tour.service.ts
+++ src/app/services/tour.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { addDoc, collection, collectionData, CollectionReference, doc, Firestore, getDoc, getDocs, query, setDoc, where } from '@angular/fire/firestore';
-import { Tour } from '../models/tour';
+import { addDoc, collection, collectionData, collectionGroup, CollectionReference, doc, Firestore, getDoc, getDocs, orderBy, query, setDoc, updateDoc, where } from '@angular/fire/firestore';
+import { Application, Review, Tour } from '../models/tour';
 import { combineLatest, firstValueFrom, Observable, of, switchMap } from 'rxjs';
 import { NotificationService } from './notification.service';
 import { map } from 'rxjs/operators';
@@ -13,6 +13,132 @@ export class TourService {
   private firestore = inject(Firestore);
 
   constructor(private notificationService: NotificationService) { }
+
+  // REFACTORING METHODS
+  async createTour(tour: Tour) {
+    return addDoc(collection(this.firestore, 'tours'), tour);
+  }
+
+  // returns null if tour does not exist
+  async getTour(tourId: string) {
+    const tourDoc = await getDoc(doc(this.firestore, 'tours', tourId));
+    return tourDoc.exists() ? { id: tourDoc.id, ...tourDoc.data() } : null;
+  }
+
+  async getAllTours() {
+    const toursCollection = collection(this.firestore, 'tours');
+    const q = query(toursCollection, orderBy('date', 'desc'));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+  
+  async getClubTours(clubId: string) {
+    const toursCollection = collection(this.firestore, 'tours');
+    const q = query(toursCollection, where('clubId', '==', clubId));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+
+  // APPLICATIONS SECTION
+
+  async applyForTour(tourId: string, userId: string, userName: string, userPhoto: string) {
+    let application: Application  = {
+      userId: userId,
+      userName: userName,
+      userPhoto: userPhoto,
+      
+      status: "pending",
+      timestamp: new Date().toISOString(),
+      declinedMessage: ""
+    };
+
+    await setDoc(doc(this.firestore, `tours/${tourId}/applications`, userId), application);
+  
+    // Notify the club
+    // const tourDoc = await getDoc(doc(this.firestore, "tours", tourId));
+    // if (tourDoc.exists()) {
+    //   const { createdBy: clubId, name: tourName } = tourDoc.data();
+    //   await addDoc(collection(this.firestore, `users/${clubId}/notifications`), {
+    //     message: `New application for "${tourName}"`,
+    //     type: "application",
+    //     read: false,
+    //     createdAt: new Date()
+    //   });
+    // }
+  }
+
+  async getTourApplications(tourId: string) {
+    const applicationsCollection = collection(this.firestore, `tours/${tourId}/applications`);
+    const querySnapshot = await getDocs(applicationsCollection);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+
+  // returns null if user did not apply
+  async getUserApplication(tourId: string, userId: string) {
+    const appDoc = await getDoc(doc(this.firestore, `tours/${tourId}/applications`, userId));
+    return appDoc.exists() ? appDoc.data() : null;
+  }
+
+  async updateApplicationStatus(tourId: string, userId: string, status: 'pending' | 'accepted' | 'declined' | 'canceled', declinedMessage?: string) {
+    await updateDoc(doc(this.firestore, `tours/${tourId}/applications`, userId), { status, declinedMessage });
+  
+    // Notify user
+    // await addDoc(collection(firestore, `users/${userId}/notifications`), {
+    //   message: `Your application for tour "${tourId}" was ${status}`,
+    //   type: status === "accepted" ? "acceptance" : "decline",
+    //   read: false,
+    //   createdAt: new Date()
+    // });
+  }
+
+  // in this case you can just get tour and read participants from there
+  // async getAcceptedParticipants(tourId: string) {
+  //   const tourRef = doc(this.firestore, `tours/${tourId}`);
+  //   const tourSnapshot = await getDoc(tourRef);
+  //   return tourSnapshot.exists() ? tourSnapshot.data()?.['participants'] || [] : [];
+  // }
+  
+  async getUserAcceptedTours(userId: string) {
+    // ne radi zato sto club id nije u participants nego u objektu
+    //throw Error('Not implemented');
+    const toursCollection = collection(this.firestore, 'tours');
+    const q = query(toursCollection, where('participantsIds', 'array-contains', userId));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+
+  async createReview(tourId: string, review: Review) {
+    await addDoc(collection(this.firestore, `tours/${tourId}/reviews`), review);
+  }
+
+  async getTourReviews(tourId: string) {
+    const reviewsCollection = collection(this.firestore, `tours/${tourId}/reviews`);
+    const querySnapshot = await getDocs(reviewsCollection);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+
+  async getUserReviews(userId: string) {
+    const reviewsQuery = query(
+      // this will fail until the index is created for all reviews subcollections
+      collectionGroup(this.firestore, 'reviews'), 
+      where('userId', '==', userId)
+    );
+    const querySnapshot = await getDocs(reviewsQuery);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+
+  //  NOTIFICATION
+  async getUserNotifications(userId: string) {
+    const snapshot = await getDocs(collection(this.firestore, `users/${userId}/notifications`));
+    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  }
+
+  async markNotificationAsRead(userId: string, notificationId: string) {
+    await updateDoc(doc(this.firestore, `users/${userId}/notifications`, notificationId), { read: true });
+  }
+
+  // END OF REFACTORING METHODS
+
 
   async loadTour(tourId: string) {
     const tourDoc = await getDoc(doc(this.firestore, 'tours', tourId));
@@ -46,7 +172,7 @@ export class TourService {
         }
 
         const userObservables = tours.map(tour =>
-          getDoc(doc(this.firestore, 'users', tour.createdBy)).then(userDoc => ({
+          getDoc(doc(this.firestore, 'users', tour.clubId)).then(userDoc => ({
             ...tour,
             createdByName: userDoc.exists() ? userDoc.data()?.['firstName'] : 'Unknown'
           }))
@@ -63,7 +189,7 @@ export class TourService {
     return addDoc(collection(this.firestore, 'tours'), tour);
   }
 
-  async applyForTour(tourId: string, tourName: string, tourOwner: string, userId: string, userName: string): Promise<void> {
+  async applyForTour2(tourId: string, tourName: string, tourOwner: string, userId: string, userName: string): Promise<void> {
     // Check if tourId or userId is invalid
     if (!tourId || !userId) {
       throw new Error('Invalid tourId or userId');
@@ -92,7 +218,7 @@ export class TourService {
     }
   }
 
-  async updateApplicationStatus(tourId: string, userId: string, status: 'accepted' | 'rejected'): Promise<void> {
+  async updateApplicationStatus2(tourId: string, userId: string, status: 'accepted' | 'rejected'): Promise<void> {
     if (!tourId || !userId) {
       throw new Error('Invalid tourId or userId');
     }
@@ -164,4 +290,6 @@ export class TourService {
       date: new Date().toISOString()
     });
   }
+
+
 }
