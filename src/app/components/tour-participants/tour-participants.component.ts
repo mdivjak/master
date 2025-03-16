05246008987f6bb13ba5402be70d4f80987d3661
mdivjak <marko.d@europe.com>
2025-03-16 18:58:58 +0100

    refactor tour participants component; add decline message functionality and modal for application rejection

--- src/app/components/tour-participants/tour-participants.component.ts
+++ src/app/components/tour-participants/tour-participants.component.ts
@@ -4,11 +4,12 @@ import { TourService } from '../../services/tour.service';
 import { Application, Tour } from '../../models/tour';
 import { NgFor, NgIf } from '@angular/common';
 import { NotificationService } from '../../services/notification.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tour-participants',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './tour-participants.component.html',
   styleUrl: './tour-participants.component.css'
 })
@@ -16,6 +17,9 @@ export class TourParticipantsComponent {
   tourId!: string;
   tour!: Tour;
   applications!: Application[];
+  declineMessage: string = '';
+  selectedApplication: Application | null = null;
+  showModal: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -53,12 +57,23 @@ export class TourParticipantsComponent {
     }
   }
 
-  async rejectApplication(application: Application) {
-    await this.tourService.updateApplicationStatus(this.tourId, application.userId, "declined", "");
-    this.tourService.removeTourParticipant(this.tourId, application.userId);
-    this.notificationService.sendNotification(application.userId, 'statusUpdate', `Your application for ${this.tour.name} has been rejected`);
+  async rejectApplication() {
+    await this.tourService.updateApplicationStatus(this.tourId, this.selectedApplication!.userId, "declined", this.declineMessage);
+    this.tourService.removeTourParticipant(this.tourId, this.selectedApplication!.userId);
+    this.notificationService.sendNotification(this.selectedApplication!.userId, 'statusUpdate', `Your application for ${this.tour.name} has been rejected: ${this.declineMessage}`);
     this.applications = await this.getApplications() as unknown as Application[];
-    // TODO: Add logic to send a decline message
+    this.declineMessage = ''; // Reset the message
+    this.selectedApplication = null; // Reset the selected application
+    this.showModal = false; // Hide the modal
+  }
+
+  openModal(application: Application) {
+    this.selectedApplication = application;
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
   }
 
   async acceptApplication(application: Application) {
